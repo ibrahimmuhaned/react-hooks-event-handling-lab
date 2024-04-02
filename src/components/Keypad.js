8eb4d4d202acd055cdfbb89665707601a8fe5aaa
ibrahimmuhaned <ibrahimmuhaned999@gmail.com>
2024-04-03 02:34:00 +0300

    done

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,24 @@
-// Code Keypad Component Here
+// // Code Keypad Component Here
 
-function Keypad (){
-    return (
-        <div></div>
-    )
+// function Keypad (){
+//     return (
+//         <div></div>
+//     )
+// }
+
+// export default Keypad;
+import React from 'react';
+
+function Keypad() {
+  function handleChange() {
+    console.log('Entering password...');
+  }
+
+  return (
+    <div>
+      <input type="password" onChange={handleChange} />
+    </div>
+  );
 }
 
 export default Keypad;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+
+// import Keypad from './components/Keypad';
+// import EyesOnMe from './components/EyesOnMe';
+
+// ReactDOM.render(
+//   <div>
+//     <Keypad />
+//     <EyesOnMe />
+//   </div>,
+//   document.getElementById('root')
+// );
+
+// index.js
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import Keypad from './components/Keypad';
 import EyesOnMe from './components/EyesOnMe';
 
